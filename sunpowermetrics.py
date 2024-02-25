3f0b65e1dfcbbbf1456fd636af033cb7778192c5
Tech Nerd <eric@thetechnerdit.com>
2024-02-24 19:07:30 -0700

    Rewrote the sunpowermetrics script to include auth and metrics into a single script using the packages I wrote.

--- sunpowermetrics.py
+++ sunpowermetrics.py
@@ -2,21 +2,21 @@ from sunpowermetrics.apimetrics import SunPowerAPI
 import datetime as datetime
 from datastores.influxdb import InfluxDb
 import config
-import json
+import logging
+import time
+from sunpowerauth.sunpowerauth import SunPowerAuth
+
+influx = InfluxDb(influxdb=config.db_url,
+                  bucket=config.db_bucket,
+                  org=config.db_org,
+                  token=config.db_token)
+power = SunPowerAPI(fqdn=config.sunpower_api_url)
+spa = SunPowerAuth()
 
 
-def main():
-    fh = open(config.sunpower_token_file, 'r')
-    token = fh.read()
-    starttime = datetime.datetime.now() - datetime.timedelta(hours=1)
-    endtime = datetime.datetime.now()
-    influx = InfluxDb(influxdb=config.db_url,
-                      bucket=config.db_bucket,
-                      org=config.db_org,
-                      token=config.db_token)
-    lg = open(config.metrics_log_location, 'a')
-    power = SunPowerAPI(fqdn=config.sunpower_api_url,
-                        token=token)
+def solarmetrics(token=None):
+    starttime = datetime.datetime.now() - datetime.timedelta(minutes=10)
+    endtime = datetime.datetime.now() - datetime.timedelta(minutes=5)
     solarquery = {"operationName": "FetchPowerData",
                   "variables": {"siteKey": config.sunpower_site_id,
                                 "interval": "five_minute",
@@ -25,7 +25,7 @@ def main():
                                 },
                   "query": "query FetchPowerData($interval: String!, $end: String!, $start: String!, $siteKey: String!) {\n  energy(interval: $interval, end: $end, start: $start, siteKey: $siteKey) {\n    energyDataSeries {\n      production\n      consumption\n      storage\n      grid\n      __typename\n    }\n    totalProduction\n    totalConsumption\n    energyMixPercentage\n    totalGridImport\n    totalGridExport\n    netGridImportExport\n    totalStorageCharged\n    totalStorageDischarged\n    netStorageChargedDischarged\n    __typename\n  }\n}\n"
                   }
-    energydata = power.getmetrics(query=solarquery)
+    energydata = power.getmetrics(query=solarquery, token=token)
     results = power.parsedata(data=energydata)
     production = []
     consumption = []
@@ -33,7 +33,6 @@ def main():
         time1 = datetime.datetime.strptime(line, "%Y-%m-%dT%H:%M:%S")
         timestamp = time1.strftime("%Y-%m-%dT%H:%M:%S-07:00")
         production.append({"timestamp": timestamp, "value": results['production'][line]['kwh'], "type": "production"})
-        lg.writelines(json.dumps({"timestamp": timestamp, "value": results['production'][line]['kwh'], "type": "production"}) + "\n")
         metric = float(results['production'][line]['kwh'])
         influx.writedata(measurement_name="production",
                          tag='"system" , "solar"',
@@ -43,14 +42,22 @@ def main():
         time1 = datetime.datetime.strptime(line, "%Y-%m-%dT%H:%M:%S")
         timestamp = time1.strftime("%Y-%m-%dT%H:%M:%S-07:00")
         consumption.append({"timestamp": timestamp, "value": results['consumption'][line]['kwh'], "type": "consumption"})
-        lg.writelines(json.dumps({"timestamp": timestamp, "value": results['consumption'][line]['kwh'], "type": "consumption"}) + "\n")
         cmetric = float(results['consumption'][line]['kwh'])
         influx.writedata(measurement_name="consumption",
                          tag='"system" , "solar"',
                          metric=cmetric,
                          timestamp=timestamp)
-    lg.close()
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            fh = open(config.sunpower_token_file, 'r')
+            token = fh.read()
+            fh.close()
+            solarmetrics(token=token)
+        except Exception as e:
+            print("Failed pulling new token")
+            solarmetrics(spa.gettoken())
+            print("Error: {}".format(e))
+        time.sleep(600)

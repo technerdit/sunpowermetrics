cd20cc9352f7ce2dc0637a246134f91fd04727f2
Tech Nerd <eric@thetechnerdit.com>
2024-02-24 19:05:15 -0700

    Added config.py to store global settings

---
+++ config.py
@@ -0,0 +1,18 @@
+# Influx DB Settings
+db_token = <INFLUXDB TOKEN>
+db_url = <INFLUXDB HOST:PORT>
+db_bucket = <INFLUXDB BUCKET>
+db_org = <INFLUXDB ORG>
+
+# Sunpower Settings
+
+sunpower_api_url = "https://edp-api-graphql.edp.sunpower.com" #This is the current fqdn for metrics
+sunpower_token_file = ".token"
+sunpower_site_id = "A_123456" # Not a real siteid
+sunpower_idp_url = "https://login.mysunpower.com/login/login.htm"
+sunpower_email = <SUNPOWER EMAIL ADDR> # This is your username
+sunpower_password = <SUNPOWER PASSWORD>
+
+# Metrics Application Settings
+
+metrics_log_location = "/var/log/sunpower/metrics.log" # Syslog file
